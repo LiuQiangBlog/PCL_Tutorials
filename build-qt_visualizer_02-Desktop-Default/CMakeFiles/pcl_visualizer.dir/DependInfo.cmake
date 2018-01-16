d6260e8b78de26704d41fd66224bac89f85646c0
liuqiang <liuqiang@liuqiang-Ubuntu16.04>
2018-01-16 16:13:23 +0800

    add pcl_visualizer example

---
+++ build-qt_visualizer_02-Desktop-Default/CMakeFiles/pcl_visualizer.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liuqiang/Documents/My_Repositories/PCL_Tutorials/qt_visualizer_02/main.cpp" "/home/liuqiang/Documents/My_Repositories/PCL_Tutorials/build-qt_visualizer_02-Desktop-Default/CMakeFiles/pcl_visualizer.dir/main.cpp.o"
+  "/home/liuqiang/Documents/My_Repositories/PCL_Tutorials/build-qt_visualizer_02-Desktop-Default/pcl_visualizer_automoc.cpp" "/home/liuqiang/Documents/My_Repositories/PCL_Tutorials/build-qt_visualizer_02-Desktop-Default/CMakeFiles/pcl_visualizer.dir/pcl_visualizer_automoc.cpp.o"
+  "/home/liuqiang/Documents/My_Repositories/PCL_Tutorials/qt_visualizer_02/pclviewer.cpp" "/home/liuqiang/Documents/My_Repositories/PCL_Tutorials/build-qt_visualizer_02-Desktop-Default/CMakeFiles/pcl_visualizer.dir/pclviewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/liuqiang/Documents/My_Repositories/PCL_Tutorials/qt_visualizer_02"
+  "/usr/local/include/vtk-7.1"
+  "/usr/local/include/pcl-1.8"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "/home/liuqiang/Qt5.9.3/5.9.3/gcc_64/include"
+  "/home/liuqiang/Qt5.9.3/5.9.3/gcc_64/include/QtWidgets"
+  "/home/liuqiang/Qt5.9.3/5.9.3/gcc_64/include/QtGui"
+  "/home/liuqiang/Qt5.9.3/5.9.3/gcc_64/include/QtCore"
+  "/home/liuqiang/Qt5.9.3/5.9.3/gcc_64/./mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
